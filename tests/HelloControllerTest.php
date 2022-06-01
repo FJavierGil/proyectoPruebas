b76f05e197ccff0cc48e328d327c70eea654fe9f
Javier Gil <franciscojavier.gil@upm.es>
2022-06-01 19:17:54 +0200

    php bin/console make:test WebTestCase HelloControllerTest

---
+++ tests/HelloControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HelloControllerTest extends WebTestCase
+{
+    public function testIndex_200_Ok(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request(
+            'GET',
+            '/hello'
+        );
+        $cuerpoRespuesta = $client->getResponse()->getContent();
+
+        $this->assertResponseIsSuccessful();
+        self::assertJson($cuerpoRespuesta);
+        $datos = json_decode($cuerpoRespuesta, true);
+        self::assertArrayHasKey('message', $datos);
+    }
+}
